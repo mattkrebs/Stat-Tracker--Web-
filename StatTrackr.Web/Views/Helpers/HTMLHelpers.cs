0eefa0eb944134b60612feef22835959a716c3d8
Ryan Drost <ryan.s.drost@gmail.com>
2011-11-19 09:50:42 -0600

    Added the MUSE Layout set and navigation on selected page

---
+++ StatTrackr.Web/Views/Helpers/HTMLHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcHtmlHelpers
+{
+    public static class HTMLHelpers
+    {
+        static string selectedClass = "class=\"active iconed\"";
+
+        public static MvcHtmlString SelectedNavItem(this HtmlHelper helper, string page1, string page2)
+        {
+
+            if (page1.Equals(page2))
+                return new MvcHtmlString(selectedClass);
+            else
+                return new MvcHtmlString(string.Empty);
+        }
+    }
+}
