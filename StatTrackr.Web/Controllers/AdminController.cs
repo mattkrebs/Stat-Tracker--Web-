32db77bce3247e2508188b11a8b3e3aec34a9663
Ryan Drost <ryan.s.drost@gmail.com>
2011-11-21 20:09:20 -0600

    admin side view templates and controller pointing to them

--- StatTrackr.Web/Controllers/AdminController.cs
+++ StatTrackr.Web/Controllers/AdminController.cs
@@ -24,5 +24,70 @@ namespace StatTrackr.Web.Controllers
             return View();
         }
 
+        public ActionResult Stats()
+        {
+            if (!StatTrackr.Framework.Security.CodeFirstSecurity.IsAuthenticated)
+            {
+                return Redirect("/account/logon");
+            }
+
+            ViewBag.Title = "Stats";
+            ViewBag.selectedPage = "stats";
+
+            return View();
+        }
+
+        public ActionResult Teams()
+        {
+            if (!StatTrackr.Framework.Security.CodeFirstSecurity.IsAuthenticated)
+            {
+                return Redirect("/account/logon");
+            }
+
+            ViewBag.Title = "Teams";
+            ViewBag.selectedPage = "teams";
+
+            return View();
+        }
+
+        public ActionResult Games()
+        {
+            if (!StatTrackr.Framework.Security.CodeFirstSecurity.IsAuthenticated)
+            {
+                return Redirect("/account/logon");
+            }
+
+            ViewBag.Title = "Games";
+            ViewBag.selectedPage = "games";
+
+            return View();
+        }
+
+        public ActionResult Divisions()
+        {
+            if (!StatTrackr.Framework.Security.CodeFirstSecurity.IsAuthenticated)
+            {
+                return Redirect("/account/logon");
+            }
+
+            ViewBag.Title = "Divisions";
+            ViewBag.selectedPage = "divisions";
+
+            return View();
+        }
+
+        public ActionResult Players()
+        {
+            if (!StatTrackr.Framework.Security.CodeFirstSecurity.IsAuthenticated)
+            {
+                return Redirect("/account/logon");
+            }
+
+            ViewBag.Title = "Players";
+            ViewBag.selectedPage = "players";
+
+            return View();
+        }
+
     }
 }
